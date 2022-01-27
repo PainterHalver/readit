97bfb2529c0a70012b440528298e2d23a151cac2
PainterHalver <73751932+PainterHalver@users.noreply.github.com>
2022-01-27 12:42:08 +0700

    client search sub

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,13 +1,22 @@
 import Axios from "axios";
+import Image from "next/image";
 import Link from "next/link";
-import { Fragment } from "react";
+import { useRouter } from "next/router";
+import { Fragment, useEffect, useState } from "react";
 
 import { useAuthDispatch, useAuthState } from "../context/auth";
+import { Sub } from "../types";
 import RedditLogo from "./../images/reddit.svg";
 
 const Navbar: React.FC = () => {
+  const [name, setName] = useState("");
+  const [subs, setSubs] = useState<Sub[]>([]);
+  const [timer, setTimer] = useState(null);
+
   const { authenticated, loading } = useAuthState();
   const dispatch = useAuthDispatch();
+
+  const router = useRouter();
 
   const logout = () => {
     Axios.get("/auth/logout")
@@ -16,6 +25,34 @@ const Navbar: React.FC = () => {
         window.location.reload(); // not passing true, soft reload?
       })
       .catch(console.log);
+  };
+
+  useEffect(() => {
+    if (name.trim() === "") {
+      return setSubs([]);
+    }
+    searchSubs();
+  }, [name]);
+
+  // Only send request after text state changes 0.25s to reduce request to serevr
+  const searchSubs = async () => {
+    clearTimeout(timer);
+    setTimer(
+      setTimeout(async () => {
+        try {
+          const { data } = await Axios.get(`/subs/search/${name}`);
+          setSubs(data.data);
+          console.log(data.data);
+        } catch (error) {
+          console.log(error);
+        }
+      }, 250)
+    );
+  };
+
+  const goToSub = (subName: string) => {
+    router.push(`/r/${subName}`);
+    setName("");
   };
 
   return (
@@ -32,13 +69,38 @@ const Navbar: React.FC = () => {
         </span>
       </div>
       {/* Search Input */}
-      <div className="flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
+      <div className="relative flex items-center mx-auto bg-gray-100 border rounded hover:border-blue-500 hover:bg-white">
         <i className="pl-4 pr-3 text-gray-500 fas fa-search"></i>
         <input
           type="text"
           className="py-1 pr-3 bg-transparent rounded w-160 focus:outline-none"
           placeholder="Search"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
+        <div
+          className="absolute left-0 right-0 bg-white"
+          style={{ top: "100%" }}
+        >
+          {subs?.map((sub) => (
+            <div
+              className="flex items-center px-4 py-3 cursor-pointer hover:bg-gray-200"
+              onClick={() => goToSub(sub.name)}
+            >
+              <Image
+                src={sub.imageUrl}
+                alt="Sub"
+                height={(8 * 16) / 4}
+                width={(8 * 16) / 4}
+                className="rounded-full "
+              ></Image>
+              <div className="ml-4 text-sm">
+                <p className="font-medium">{sub.name}</p>
+                <p className="text-gray-600">{sub.title}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
 
       {/* Auth buttons */}
