7a4ffda1ca944bcee8072cfb504429138c2e1a19
PainterHalver <73751932+PainterHalver@users.noreply.github.com>
2022-02-09 13:58:22 +0700

    cookie refresh on page reload fix1

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -2,6 +2,7 @@ import { isEmpty, validate } from "class-validator";
 import { Request, Response, NextFunction } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import cookie from "cookie";
 
 import User from "../entities/User";
 import catchAsync from "../utils/catchAsync";
@@ -97,15 +98,24 @@ export const login = catchAsync(
     const token = jwt.sign({ id: user._id, username }, process.env.JWT_SECRET!); // ! for required (removing null check)
 
     // Send jwt to browser as cookie
-    res.cookie("jwt", token, {
-      httpOnly: true, // cannot be changed in anyway by browser
-      secure: process.env.NODE_ENV === "production" ? true : false, // hard to https in development
-      domain:
-        process.env.NODE_ENV === "development" ? ".localhost" : ".vercel.app",
-      maxAge: 360000, // 100 hours
-      // sameSite: "strict",
-      //   path: "/", // all routes (but here is from /api/auth/)
-    });
+    // res.cookie("jwt", token, {
+    //   httpOnly: true, // cannot be changed in anyway by browser
+    //   secure: process.env.NODE_ENV === "production" ? true : false, // hard to https in development
+    //   domain:
+    //     process.env.NODE_ENV === "development" ? ".localhost" : ".vercel.app",
+    //   maxAge: 360000, // 100 hours
+    //   // sameSite: "strict",
+    //   //   path: "/", // all routes (but here is from /api/auth/)
+    // });
+    res.set(
+      "Set-Cookie",
+      cookie.serialize("jwt", token, {
+        httpOnly: true,
+        sameSite: "strict",
+        maxAge: 360000,
+        path: "/",
+      })
+    );
 
     return res.status(200).json({
       status: "success",
