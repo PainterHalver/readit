6c612765c8772e85a8c8c74ccbc17ea8a48de63e
painterhalver <hieptrang13042002@gmail.com>
2022-07-18 08:22:02 +0700

    Remove react-loading-skeleton

--- client/src/components/TopSubSkeleton.tsx
+++ client/src/components/TopSubSkeleton.tsx
@@ -1,12 +1,25 @@
-import Skeleton from "react-loading-skeleton";
-import "react-loading-skeleton/dist/skeleton.css";
-
 export default function TopSubSkeleton() {
   return (
     <div>
-      <div className="flex items-center px-4 py-2 text-xs border-b">
-        <Skeleton circle width={27} height={27} className="mr-2" />
-        <Skeleton width={250} />
+      <div className="flex items-center px-4 py-2 text-xs border-b dark:border-dark-border">
+        <div className="mr-2 bg-gray-300 rounded-full h-7 w-7 animate-pulse dark:bg-dark-border"></div>
+        <div className="w-11/12 h-4 bg-gray-300 rounded dark:bg-dark-border animate-pulse"></div>
+      </div>
+      <div className="flex items-center px-4 py-2 text-xs border-b dark:border-dark-border">
+        <div className="mr-2 bg-gray-300 rounded-full h-7 w-7 animate-pulse dark:bg-dark-border"></div>
+        <div className="w-11/12 h-4 bg-gray-300 rounded dark:bg-dark-border animate-pulse"></div>
+      </div>
+      <div className="flex items-center px-4 py-2 text-xs border-b dark:border-dark-border">
+        <div className="mr-2 bg-gray-300 rounded-full h-7 w-7 animate-pulse dark:bg-dark-border"></div>
+        <div className="w-11/12 h-4 bg-gray-300 rounded dark:bg-dark-border animate-pulse"></div>
+      </div>
+      <div className="flex items-center px-4 py-2 text-xs border-b dark:border-dark-border">
+        <div className="mr-2 bg-gray-300 rounded-full h-7 w-7 animate-pulse dark:bg-dark-border"></div>
+        <div className="w-11/12 h-4 bg-gray-300 rounded dark:bg-dark-border animate-pulse"></div>
+      </div>
+      <div className="flex items-center px-4 py-2 text-xs border-b rounded-b dark:border-dark-border">
+        <div className="mr-2 bg-gray-300 rounded-full h-7 w-7 animate-pulse dark:bg-dark-border"></div>
+        <div className="w-11/12 h-4 bg-gray-300 rounded dark:bg-dark-border animate-pulse"></div>
       </div>
     </div>
   );
